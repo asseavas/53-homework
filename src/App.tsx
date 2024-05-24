e4728e3d775a86206e73c20ca9c6e9b4ef2e66d5
Klimova Alina <alinaklimova82@gmail.com>
2024-05-24 18:48:08 +0600

    Added logic to the App. Added the 'uuid' library.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import React, { useState } from 'react';
+import AddTaskForm from './components/AddTaskForm/AddTaskForm';
+import Task from './components/Task/Task';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
-const App = () => {
 
+interface Task {
+  id: string;
+  text: string;
+}
+
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: uuidv4(), text: 'Eat' },
+    { id: uuidv4(), text: 'Sleep' },
+    { id: uuidv4(), text: 'Walk' }
+  ]);
+
+  const addTask = (taskText: string) => {
+    setTasks([...tasks, { id: uuidv4(), text: taskText }]);
+  };
+
+  const deleteTask = (id: string) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  return (
+    <div className="container">
+      <AddTaskForm addTask={addTask} />
+      {tasks.map(task => (
+        <Task key={task.id} id={task.id} text={task.text} deleteTask={deleteTask} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
